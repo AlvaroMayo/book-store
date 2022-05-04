f17f2d451977ec560d46474c0a0fc550890c82d8
Alvaro Mayo <alvaro.mayo@esure.com>
2022-05-04 12:08:05 +0100

    feat: create star rating funct

--- src/component/Book.tsx
+++ src/component/Book.tsx
@@ -1,6 +1,8 @@
 import { Grid } from "@mui/material";
 import { getSnapshot } from "mobx-state-tree";
 import { setupRootStore } from "../service/store/setup";
+import  StarBorderIcon from '@mui/icons-material/StarBorder';
+import  StarIcon from '@mui/icons-material/Star';
 
 type book = {
   author: string;
@@ -14,17 +16,27 @@ type book = {
 export const Book = ({ name, author, cover, id, price, rating }: book) => {
   const addToCart = (book: any) => {
     rootTree.store.addToCart(book);
-    console.log(getSnapshot(rootTree.store.cart));
+    console.log(getSnapshot(rootTree));
   };
 
   const { rootTree } = setupRootStore();
+  let stars = [];
+  for(let i=0; i<5; i++) {
+    if(i<rating) {
+        stars.push("StarIcon")
+    } else {
+        stars.push("StarIconBorder")
+    }
+  }
 
   return (
     <Grid container direction={"column"}>
       <img style={{maxHeight:"240px", height:"100%"}} src={`/img${cover}`} alt="" />
       <h5>{name}</h5>
       <span>by {author}</span>
-      <span>stars</span>
+      <span>
+        {stars.map((star, i) => star === `StarIcon` ? <StarIcon key={i}/> : <StarBorderIcon key={i}/>)}
+      </span>
       <span>{price}</span>
       <Grid item>
         <button
